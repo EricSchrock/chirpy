19ac3332da3dcb8a851caddb347045ee7141545e
Eric Schrock <eric.schrock22@gmail.com>
2024-08-12 17:18:16 -0400

    Merge main.go and cors.go

--- main.go
+++ main.go
@@ -36,3 +36,16 @@ func main() {
 	err := server.ListenAndServe()
 	log.Fatal(err)
 }
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
